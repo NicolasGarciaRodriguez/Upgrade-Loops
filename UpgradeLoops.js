828c4c1d0656d5c8bb3fb7b91d46c848113233f1
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-22 11:27:27 +0200

    Usa includes

---
+++ UpgradeLoops.js
@@ -0,0 +1,9 @@
+// 1. Usa Includes
+
+// const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+// for (product of products) {
+
+//     console.log(product.includes("Camiseta"))
+
+// }
