4bb098fc4bf99ba680ee57c9cfaf237e16c05cf8
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-25 13:00:24 +0200

    Probando for of

--- UpgradeLoops.js
+++ UpgradeLoops.js
@@ -7,3 +7,37 @@
 //     console.log(product.includes("Camiseta"))
 
 // }
+
+
+// 2. Condicionales avanzados
+
+// const alumnos = [
+
+//         {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+// 		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+// 		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+// 		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+// 		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+// ]
+
+
+// for (alumno of alumnos) {
+
+// 	for (prop of alumno){
+
+// 		console.log(prop.includes(true)) 
+
+// 	}
+	
+// }
+
+
+// 3. Probando for of
+
+// const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+// for (places of placesToTravel) {
+
+// 	console.log(places)
+
+// }
