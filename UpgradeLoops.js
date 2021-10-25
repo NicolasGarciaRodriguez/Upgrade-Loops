6f99edfe32ea13293b0554a30a980ac47adb9f2e
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-25 13:09:08 +0200

    Probando for in

--- UpgradeLoops.js
+++ UpgradeLoops.js
@@ -13,7 +13,7 @@
 
 // const alumnos = [
 
-//         {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+//		{name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
 // 		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
 // 		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
 // 		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
@@ -39,5 +39,21 @@
 // for (places of placesToTravel) {
 
 // 	console.log(places)
+
+// }
+
+
+// 4. Probando for in 
+
+// const alien = {
+//     name: 'Wormuck',
+//     race: 'Cucusumusu',
+//     planet: 'Eden',
+//     weight: '259kg'
+// }
+
+// for (alienProps in alien) {
+
+// 	console.log(alienProps + ":" + alien[alienProps])
 
 // }
